a5a63cc0846d9221345d1096a4d8ab7713180754
Pradeep <117845455+PradeepCodes@users.noreply.github.com>
2025-05-10 16:04:06 +0530

    Update Basics_02_To_Check_the_Composite_Number.java
    Prime Number alternate Solution Submitted

--- Basics/Basics_02_To_Check_the_Composite_Number.java
+++ Basics/Basics_02_To_Check_the_Composite_Number.java
@@ -33,6 +33,41 @@ public class Basics_02_To_Check_the_Composite_Number {
     }
 }
 
+//Alternate Solution
+===================
+//package Src;
+
+// import java.util.Scanner;
+
+// public class PrimeNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int num=0;
+//         for(int i=1;i<=a;i++)
+//         {
+//             float value = a%i;
+//             if(value == 0)
+//             {
+
+//                 num=num+1;
+//             }
+//         }
+//         if(num <= 1)
+//         {
+//             System.out.println("nor Prime Number either composite");
+//         }
+//         if(num ==2)
+//         {
+//             System.out.println("Prime Number");
+//         }
+//         else{
+//             System.out.println("Composte Number");
+//         }
+//     }
+// }
+
+
 //
 //Given a number N, print 'yes' if it is composite else print 'no'.
 //Sample Testcase :
